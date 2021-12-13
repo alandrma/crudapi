b7e44a0b68accb7d02d7caa6c4878d27cf3eb588
alandrma <darma882@gmail.com>
2021-12-13 14:55:35 +0700

    add paginantion

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,14 @@
 class UserMailer < ApplicationMailer
-    def welcome_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    default from: 'notifications@example.com'
+
+    def welcome_email
+      @user = params[:user]
+      @url  = 'http://example.com/login'
+      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+end
+  #   def welcome_email(user)
+  #   @user = user
+  #   mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  # end
 end
